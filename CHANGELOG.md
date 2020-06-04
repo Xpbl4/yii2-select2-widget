304d85b47576bc6751aa0d022dfdeb77b0034975
Serge Mashkov <serge.mashkov@gmail.com>
2020-06-04 14:14:03 +0700

    Initial release (Xpbl4)

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All Notable changes to yii2-select2-widget will be documented in this file.
+
+## 0.1.0
+
+- Initial release (Xpbl4)
---
+++ src/Select2BootstrapAsset.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace xpbl4\widgets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Widget bootstrap asset bundle.
+ */
+class Select2BootstrapAsset extends AssetBundle
+{
+	/**
+	 * @inheritdoc
+	 */
+	public $sourcePath = '@bower/select2';
+
+	/**
+	 * @inheritdoc
+	 */
+	public $css = [
+		'select2-bootstrap.css'
+	];
+
+	/**
+	 * @inheritdoc
+	 */
+	public $depends = [
+		'xpbl4\widgets\Select2Asset',
+		'yii\bootstrap\BootstrapAsset'
+	];
+}
